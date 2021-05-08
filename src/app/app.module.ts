dc36d2ce21c697a3afdea15601690ff204b257ab
saiprudvish <rsaiprudvish123@gmail.com>
2021-05-08 14:24:20 +0530

    new to git

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { MyFirstComponent } from './my-first/my-first.component';
+import { CardOneComponent } from './card-one/card-one.component';
+import { CardTwoComponent } from './card-two/card-two.component';
+import { CardThreeComponent } from './card-three/card-three.component';
+import { CardFourComponent } from './card-four/card-four.component';
+import { CardFiveComponent } from './card-five/card-five.component';
+import { CardSixComponent } from './card-six/card-six.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponent,
+    CardOneComponent,
+    CardTwoComponent,
+    CardThreeComponent,
+    CardFourComponent,
+    CardFiveComponent,
+    CardSixComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
